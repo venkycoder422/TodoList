f561f8894e3c4991ba8e6924edf35c9287f31e01
venky422 <dommaravenkatesulu22@gmail.com>
2022-11-19 13:56:02 +0530

    added edit and delete button

--- Scripts/todoList.js
+++ Scripts/todoList.js
@@ -9,7 +9,7 @@ const getTasks=async()=>{
         console.log(error);
     }
 }
-getTasks();
+
 
 // POST DATA
 const addTask = async()=>{
@@ -49,21 +49,40 @@ const  updateOnUi = (data)=>{
     data.forEach(function(text){
         console.log(text);
         let div=document.createElement('div');
-
+        let div2=document.createElement('div');
+        let div3=document.createElement('div');
+        div3.setAttribute('id',"buttonsDiv")
         div.setAttribute('id',"listsContainer")
-        let tr=document.createElement('h3');
-        tr.setAttribute('id',"listsData");
+        
+        let title=document.createElement('h2');
+        title.setAttribute('id',"listsData");
+        let addButton = document.createElement('button');
+        addButton.innerText="Edit"
+        addButton.setAttribute('id',"editbutton");
+        let deleteButton = document.createElement('button');
+        deleteButton.setAttribute('id',"deletebutton");
+        
+        deleteButton.innerText="Delete"
         if(text.status===true){
-            tr.style.color="green";
+            title.style.color="green";
         }else{
-            tr.style.color="red";
+            title.style.color="red";
         }
         
+        div2.append(title);
+        div3.append(addButton,deleteButton)
         
-        tr.innerText=text.title;
+        title.innerText=text.title;
 
-        div.append(tr);
+        div.append(div2,div3);
         document.querySelector('#container').append(div);
 
     });
+        const deleteTask=()=>{
+            console.log("CALLING DELETE")
+        }
+
+        var d = document.querySelector("#deletebutton");
+        d.addEventListener("click",deleteTask());
+    
 }
